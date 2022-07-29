f4598f5629e1b44358d025d638e224186de37084
Nathan Moore <n@nmoore.net>
2022-07-29 15:10:33 -0600

    store state in Board

--- src/index.js
+++ src/index.js
@@ -12,7 +12,9 @@ class Square extends React.Component {
 
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'}) }>
+      <button
+        className="square"
+        onClick={() => this.setState({value: 'X'}) }>
         {this.state.value}
       </button>
     );
@@ -20,8 +22,20 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
